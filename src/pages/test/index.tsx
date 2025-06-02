a1128dc044721c3b103e5864b5c2c7b19edd8de4
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-06-02 19:18:13 +0900

    テストページに画像を追加し、スタイルを調整しました。

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Geist_Mono } from "next/font/google";
+import Image from "next/image";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -11,6 +12,16 @@ export default function TestPage() {
     <div
       className={` ${geistMono.className} flex flex-col gap-4 items-center justify-center min-h-screen pt-16`}
     >
+      <div className="w-40 h-40 relative">
+        <Image
+          src={"/bedf3ef793988a02900e4f9bec0cc8e6-15.jpg"}
+          alt="Test Image"
+          fill
+          sizes="160px"
+          className="object-cover rounded-lg"
+          priority
+        />
+      </div>
       <Link className="text-lg text-blue-600 hover:underline" href="/">
         Go back to Home
       </Link>
