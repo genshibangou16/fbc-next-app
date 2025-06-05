b1fc1e0a058c32ce07ac4907dbee7043f748e268
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-06-05 17:55:24 +0900

    予報ページに地域選択のドロップダウンを追加し、オフィス情報を取得するためのgetStaticProps関数を実装

--- src/pages/forecast/index.tsx
+++ src/pages/forecast/index.tsx
@@ -2,15 +2,37 @@ import useSWR from "swr";
 import { ForecastResponseData } from "../api/forecast/[slug]";
 import Link from "next/link";
 
+type Office = {
+  name: string;
+  enName: string;
+  officeName: string;
+  parent: string;
+  children: string[];
+};
+
+type Offices = {
+  [key: string]: Office;
+};
+
 const fetcher = (url: string): Promise<ForecastResponseData> =>
   fetch(url).then((response) => response.json());
 
-export default function ForecastPage() {
+export default function ForecastPage({ offices }: { offices: Offices }) {
   const { data }: { data: ForecastResponseData | undefined } =
     useSWR<ForecastResponseData>("/api/forecast/130000", fetcher);
   const reportDatetime = new Date(data?.reportDatetime || "");
   return (
     <div className="pt-16">
+      <div className="my-4">
+        <label htmlFor="office">地域を選択してください</label>
+        <select id="office" className="ml-2 p-2 border border-gray-300 rounded">
+          {Object.entries(offices).map(([key, office]) => (
+            <option key={key} value={key} selected={key === "130000"}>
+              {office.name} ({office.enName})
+            </option>
+          ))}
+        </select>
+      </div>
       {data && (
         <>
           <h1 className="text-2xl">
@@ -27,4 +49,16 @@ export default function ForecastPage() {
       </Link>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://www.jma.go.jp/bosai/common/const/area.json");
+  const offices: Offices = await res.json().then((data) => {
+    return data.offices as Offices;
+  });
+  return {
+    props: {
+      offices,
+    },
+  };
 }
